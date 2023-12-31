7c83534293d46e72e92bcc581dc6dbc2d569b4c4
folli14 <cesarfolli@gmail.com>
2023-12-31 02:10:45 -0300

    calculator

---
+++ projetos/calculadora/calculadora.js
@@ -0,0 +1,20 @@
+function soma(valor1, valor2) {
+  return valor1 + valor2;
+}
+
+function sub(valor1, valor2) {
+  return valor1 - valor2;
+}
+
+function mult(valor1, valor2) {
+  return valor1 * valor2;
+}
+
+function div(valor1, valor2) {
+  return valor1 / valor2;
+}
+
+console.log(soma(9, 5));
+console.log(sub(8, 2));
+console.log(mult(5, 5));
+console.log(div(100 , 10));
