6bfd36571c8ebc5f5381aa05d40f8d1561d8b675
folli14 <cesarfolli@gmail.com>
2024-01-04 23:29:37 -0300

    using API

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,15 @@ const filmes = require("./src/data/filmes.json");
 server.get("/", (req, res) => {
   return res.json({ mensagem: "rodando server" });
 });
+
 //TEST 2
 server.get("/usuarios", (req, res) => {
   return res.json({ usuario: "Júlio César" });
+});
+
+//TEST 3
+server.get("/filmes", (req, res) => {
+  return res.json(filmes);
 });
 
 server.listen(3000, () => {
