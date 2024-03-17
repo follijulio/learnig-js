a3ab19fc81f8c63c7d5302c614041563fcb573ee
folli14 <cesarfolli@gmail.com>
2024-03-16 23:27:46 -0300

    hihihihihi

--- treino_inicial/brincando/somatorio.js
+++ treino_inicial/brincando/somatorio.js
@@ -1,5 +1,12 @@
-const videos = [39.16, 25.31, 24.26, 28.05, 28.54, 27.30, 29.20, 20.06, 19.57, 33.09, 26.24, 20.59, 14.43, 17.38, 22.48, 10.57, 18.56, 12.47, 16.44, 15.43];
-
+const minutos = [
+  7, 2, 5, 5, 9, 2, 5, 3, 2, 11, 2, 2, 3, 2, 3, 4, 5, 1, 1, 2, 5, 9, 11, 4, 8,
+  10, 13, 1, 5, 3, 6, 7, 5, 3, 1, 7, 4, 9, 3, 2, 6, 5, 8, 2,
+];
+const segundos = [
+  3, 29, 40, 0, 22, 41, 0, 40, 30, 41, 57, 56, 51, 46, 39, 49, 52, 23, 46, 55,
+  52, 25, 5, 13, 35, 37, 57, 40, 36, 47, 51, 25, 53, 54, 42, 59, 39, 24, 6, 59,
+  23, 17, 24, 5, 14, 4,
+];
 function soma(videos) {
   let total = 0;
   for (let i = 0; i < videos.length; i++) {
@@ -9,9 +16,12 @@ function soma(videos) {
 }
 
 function main() {
-    var valor = soma(videos)
-    valor = valor / 60
+  var valor1 = soma(minutos);
+  var valor2 = soma(segundos) / 60;
+  var valor = valor1 + valor2;
+  valor *= 1.5
+  valor /= 60
   console.log(`A soma de todos os vídeos é: ${valor} minutos.`);
 }
 
-main()
+main();
